d757ddc5f44722ba4e70b2deda64e23cb5a6af44
rinkuberde <rinkuberde@gmail.com>
2023-01-19 12:50:28 +0530

    [Rinku] - Create a Stack

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Stack And Queue practice Problems!!!");
+            Console.WriteLine("\t\t\t\t\tData Structures : Stacks and Queues");
+            Console.WriteLine();
+            Console.WriteLine("-------------Stack Operations-------------");
+            Console.WriteLine("1.Push operation");
+            Console.WriteLine("------------------------------------------");
+            Console.Write("Enter the option : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            LinkedListStack linkedListStack = new LinkedListStack();
+            switch (num)
+            {
+                case 1:
+                    linkedListStack.Push(70);
+                    linkedListStack.Push(30);
+                    linkedListStack.Push(56);
+                    linkedListStack.Display();
+                    break;
+            }
         }
     }
 }
